93db3dc687202372253294af3efe5b0de7329838
amdymstfa <amdymoustapha011@gmail.com>
2025-03-17 12:45:23 +0000

    done: user can acess depending his role

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,12 +7,37 @@ use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 
+
 class LoginController extends Controller
 {
     
     public function showLoginForm()
     {
         return view('auth.login');
+    }
+
+    public function authenticate(Request $request): RedirectResponse{
+        // retrive datas form
+        $credentials = $request-> validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        // check crudentials
+        if (Auth::attempt($credentials)){
+            if(Auth::user()->role === 'admin'){
+                $request->session()->regenerate();
+                return redirect()->intended('dashboard');
+            }else{
+                return redirect()->intended('userProfil');
+            }
+        }
+
+        return back()->withErrors([
+
+            'email' => 'The provided credentials do not match our records.',
+
+        ])->onlyInput('email');
     }
 
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+  
+    public function showProfile()
+    {
+        return view('user.userProfil'); 
+    }
+    
+    public function showDashboard()
+    {
+        return view('admin.dashboard'); 
+    }
 }
