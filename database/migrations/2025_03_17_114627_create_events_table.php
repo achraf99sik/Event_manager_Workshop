0ead1638bc5e34d9a3a4790e15a698e7bff7ef17
achraf <achrafsikal@gmail.com>
2025-03-17 11:58:51 +0000

    feat: adding users and events migrations

--- database/migrations/2025_03_17_114627_create_events_table.php
+++ database/migrations/2025_03_17_114627_create_events_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('titre');
+            $table->text('description');
+            $table->dateTime('date');
+            $table->integer('placesDispo');
             $table->timestamps();
         });
     }
